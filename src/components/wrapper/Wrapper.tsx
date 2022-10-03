75a5c45f3b99a24ad50a2b2a72af0e8a949c3397
Elkhan <98739225+Elkhan2003@users.noreply.github.com>
2022-10-03 20:48:39 +0600

    add react router

--- src/components/wrapper/Wrapper.tsx
+++ src/components/wrapper/Wrapper.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import style from "./wrapper.module.css";
 
+import { Routes, Route, Link } from "react-router-dom";
+import { Home } from "../pages/Home";
+import { Contact } from "../pages/Contact";
+
 export const Wrapper = () => {
 	return (
 		<div>
@@ -9,7 +13,12 @@ export const Wrapper = () => {
 					<header className={style.header}>Привет</header>
 					<main className={style.main}>
 						<section>
-							<div>Body</div>
+							<div className={style.content}>
+								<Routes>
+									<Route path="/" element={<Home />} />
+									<Route path="Contact" element={<Contact />} />
+								</Routes>
+							</div>
 						</section>
 					</main>
 					<footer className={style.footer}>Конец</footer>
